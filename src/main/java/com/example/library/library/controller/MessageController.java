9b0ad18aa47f7e2d02209de8812a9c202fa0028d
eyllowboy <si-roga@yandex.ru>
2021-09-12 18:56:38 +0300

    add message for all users

--- src/main/java/com/example/library/library/controller/MessageController.java
+++ src/main/java/com/example/library/library/controller/MessageController.java
@@ -9,14 +9,13 @@ import org.hibernate.service.spi.ServiceException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.*;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/messages")
@@ -87,6 +86,48 @@ public class MessageController {
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return "messages/modal/addMessage";
+        }
+    }
+    @PostMapping("/saveMessage")
+    public String saveMessage(@RequestParam(value = "loginUser") String loginUser,
+                              Message message, HttpServletRequest request, Model model) {
+        try {
+            String userName = request.getRemoteUser();
+            Optional<User> optional = userService.getUserByLogin(userName);
+            User fromUser = optional.orElseThrow(() -> new ServiceException("Warning  Error"));
+            message.setFromUser(fromUser.getLogin());
+
+            Optional<User> optionalLogin = userService.getUserByLogin(loginUser);
+            User user = optionalLogin.orElseThrow(() -> new ServiceException("Warning  Error"));
+
+            message.setUser(user);
+
+            LocalDateTime dataMessage = LocalDateTime.now();
+            message.setDataMessage(dataMessage);
+
+            messageService.createNewMessage(message);
+
+            model.addAttribute("messages", messageService.getMessages(user));
+            model.addAttribute("message", "Сообщение успешно доставлено");
+            model.addAttribute("alertClass", "alert-success");
+            return "messages/message :: message_list";
+        }catch (ServiceException e) {
+            String userName = request.getRemoteUser();
+            Optional<User> optional = userService.getUserByLogin(userName);
+            User user = optional.orElseThrow(()-> new ServiceException("Warning  Error"));
+            model.addAttribute("messages", messageService.getMessages(user));
+            model.addAttribute("message", "Пользователь с таким логином не найден");
+            model.addAttribute("alertClass", "alert-danger");
+            return "messages/message :: message_list";
+        }
+        catch (Exception e) {
+            String userName = request.getRemoteUser();
+            Optional<User> optional = userService.getUserByLogin(userName);
+            User user = optional.orElseThrow(()-> new ServiceException("Warning  Error"));
+            model.addAttribute("messages", messageService.getMessages(user));
+            model.addAttribute("message", "Ошибка отправки сообщения");
+            model.addAttribute("alertClass", "alert-danger");
+            return "messages/message :: message_list";
         }
     }
 }
